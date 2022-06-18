726f78bf7cde9ddd7adea1793b51cac91190cb5a
kipyegon <kenmisik@gmail.com>
2022-06-18 13:37:25 +0300

    If and elif statements

---
+++ temp.py
@@ -0,0 +1,12 @@
+msg = "Hello World"
+print(msg)
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else: 
+    print("It's cold")
+
+print("Done")
