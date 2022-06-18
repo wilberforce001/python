29eb61362ea57ca62568e151d37481a4d7cea8e9
kipyegon <kenmisik@gmail.com>
2022-06-18 14:35:39 +0300

    in python logical operators are short-circuit

--- eligibility.py
+++ eligibility.py
@@ -1,3 +1,13 @@
-age = 12
-message = "Eligible" if age >=18 else "Not eligble"
+age = 22
+message = "Eligible" if age >=18 else "Not eligible"
 print (message)
+
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+
