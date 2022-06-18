2d4da61adf854d750ce02e627692c9d1070a3a7e
kipyegon <kenmisik@gmail.com>
2022-06-18 15:15:41 +0300

    chaining comparison operators

--- eligibility.py
+++ eligibility.py
@@ -11,3 +11,20 @@ if (high_income or good_credit) and not student:
 else:
     print("Not eligible")
 
+#In python logical operators are short-circuit. Meaning that when python interpreter wants to evaluate an expression it starts with the first argument and checks for one hat is true.#
+
+#Chaining Comparison Operators 
+# age should be between 18 and 65
+age = 17
+if 18 <= age < 65:
+    print("Eligible")
+else:
+    print("Not eligible") 
+
+# Quiz
+if 10 == "10":
+    print ("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
