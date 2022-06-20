202d65f621e7806260e4e68841d4267d83b89fd4
kipyegon <kenmisik@gmail.com>
2022-06-20 11:01:27 +0300

    Hello John, happy 25th birthday!

---
+++ birthday.py
@@ -0,0 +1,17 @@
+people = [{'name': 'John', 'age':25}, {'name': 'Sarah', 'age':23}, {'name': 'Lisa', 'age':101}]
+for person in people:
+    name = person['name']
+    age = person['age']
+    if str(age)[-1]=='1': #convert integer to string, get the last character from the string
+        Variable_text = 'st'
+    elif str(age)[-1]=='2':
+        Variable_text = 'nd'
+    elif str(age)[-1]=='3':
+        Variable_text = 'rd'
+    else:
+        Variable_text = 'th'
+    
+    message = 'Hello ' + name + ', happy ' + str(age) + Variable_text + ' birthday!'
+    print(message)
+
+    
