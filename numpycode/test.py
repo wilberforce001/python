1c35f1b577b7c82c6bf2d3cd3d94dd69e893168f
kipyegon <kenmisik@gmail.com>
2022-07-31 00:18:51 +0300

    Numpy lists and arrays

--- numpycode/test.py
+++ numpycode/test.py
@@ -40,9 +40,49 @@ area = np_length * np_width
 
 print(area)
 
-#Adding lists
+#Example 1
+# Adding 2 lists
 length_dim = [1.1, 20.5, 53.6, 26.7, 89.4] #length in metres
 width = [10.23, 28.15, 24.6, 33.58, 54] #width in metres
 
 sum_dim = length_dim + width
 print(sum_dim) #Append the 2 lists together, list is now longer
+
+#Example 2
+import numpy as np 
+sum_dim = np.array(length_dim) + np.array(width)
+print(sum_dim)
+
+#Convert length in meters to centimeters
+length_dim = [1.1, 20.5, 53.6, 26.7, 89.4] #length in metres
+length_cm = [l * 100 for l in length_dim]
+print(length_cm)
+
+#Revisit
+import numpy as np
+np_length = np.array(length_dim)
+np_length_cm = np_length * 100
+print(np_length_cm)
+
+#Numpy can perform multiplication on more than 1000 measurements
+#d = np.empty(1000, int) # can specify the datatype
+#print(d, "\n")
+#product = d * 2
+#print(product)
+
+
+#Generating arrays with random values
+a = np.zeros(3, int) #Arrays with zeros
+print(a)
+
+b = np.ones(5, str)
+print(b)
+
+c =np.empty(10, int)
+print(c)
+
+e = np.arange(15) #(start, stop, step)
+print(e)
+
+f =np.random.randint(5, size = (2,4))
+print(f)
