2dbb8762040a4765fed174140992d06d7490d0dd
kipyegon <kenmisik@gmail.com>
2022-07-31 11:33:34 +0300

    BMI calculation

---
+++ numpycode/BMI.py
@@ -0,0 +1,25 @@
+from email.quoprimime import quote
+
+
+height = float(input('enter height in inches: '))
+weight = float(input('enter weight in lbs: '))
+
+def BMI(height, weight):
+    bmi = weight/(height**2) * 703
+    if (bmi < 16):
+        return 'severely underweight', bmi
+    
+    elif (bmi >= 16 and bmi < 18.5):
+        return 'underweight', bmi
+
+    elif (bmi >= 18.5 and bmi < 25):
+        return "healthy", bmi
+
+    elif (bmi >= 25 and bmi < 30):
+        return 'overweight', bmi 
+    
+    elif (bmi >= 30):
+        return 'obese', bmi
+
+quote, bmi = BMI(height, weight)
+print('your bmi is: {} and you are: {}'. format(bmi, quote))
